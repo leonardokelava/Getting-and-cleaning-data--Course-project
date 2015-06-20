3ca590a7a9b6f696a4ac086412aebb202037a152
leonardokelava <leonardokelava@users.noreply.github.com>
2015-06-20 11:30:30 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,32 +1,28 @@
 
 
-url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
-if(!dir.exists("Projectdata")) {dir.create ("Projectdata")}
-if(!file.exists("./Projectdata/Dataset.zip")) {download.file(url, "./Projectdata/Dataset.zip")}
-if(!file.exists("./Projectdata/UCI HAR Dataset")) {unzip("./Projectdata/Dataset.zip",  exdir = "./Projectdata")}
 
 library(dplyr)
 library(tidyr)
 
-allvariables <- read.table("./Projectdata/UCI HAR Dataset/features.txt")
+allvariables <- read.table("./UCI HAR Dataset/features.txt")
 index <- grep("std|mean" , allvariables[,2])
 notneeded <- grep("Freq", allvariables[,2])
 indexneeded <- index[!(index%in%notneeded)]
 
-testset<- read.table("./Projectdata/UCI HAR Dataset/test/x_test.txt",  colClasses = "numeric")
+testset<- read.table("./UCI HAR Dataset/test/x_test.txt",  colClasses = "numeric")
 testset <- testset[,indexneeded]
 colnames(testset) <- allvariables[indexneeded,2]
 
-subjecttestset <-   read.table("./Projectdata/UCI HAR Dataset/test/subject_test.txt", col.names = "Subject")
-testsetactivitylabels <- read.table("./Projectdata/UCI HAR Dataset/test/y_test.txt", col.names = "Activity")
+subjecttestset <-   read.table("./UCI HAR Dataset/test/subject_test.txt", col.names = "Subject")
+testsetactivitylabels <- read.table("./UCI HAR Dataset/test/y_test.txt", col.names = "Activity")
 testset_tidy <-cbind(subjecttestset,testsetactivitylabels, testset)
 
-trainset <- read.table("./Projectdata/UCI HAR Dataset/train/x_train.txt",  colClasses = "numeric")
+trainset <- read.table("./UCI HAR Dataset/train/x_train.txt",  colClasses = "numeric")
 trainset <-trainset[,indexneeded]
 colnames(trainset) <- allvariables[indexneeded,2]
 
-subjecttrainset <-   read.table("./Projectdata/UCI HAR Dataset/train/subject_train.txt", col.names = "Subject")
-trainsetlabels <- read.table("./Projectdata/UCI HAR Dataset/train/y_train.txt", col.names = "Activity")
+subjecttrainset <-   read.table("./UCI HAR Dataset/train/subject_train.txt", col.names = "Subject")
+trainsetlabels <- read.table("./UCI HAR Dataset/train/y_train.txt", col.names = "Activity")
 trainset_tidy <-cbind(subjecttrainset,trainsetlabels, trainset)
 
 bigdata <- rbind(testset_tidy, trainset_tidy)
